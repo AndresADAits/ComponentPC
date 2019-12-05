3a045aed9a07139656fd79c3c33d0ac899879e4b
JAndres <joanferfra@hotmail.com>
2019-12-05 11:37:43 +0100

    Mareando la perdiz

--- src/clasesYobjetos.js
+++ src/clasesYobjetos.js
@@ -1,8 +1,8 @@
-class Movil{
+class Movil {
     //constructor(modelo, precio, marca, pantalla, rom, ram, bateria, camara)
     //let n3310 = new Movil(1,"n3310","Nokia","Caniche",5);
     // constructor(id,modelo,marca,pantalla,rom){
-    constructor(id,modelo,marca,pantalla,rom,ram,bateria,camara,precio){
+    constructor(id, modelo, marca, pantalla, rom, ram, bateria, camara, precio) {
         this.id = id;//Para identificarlo de forma única
         this.modelo = modelo;
         this.marca = marca;
@@ -11,105 +11,105 @@ class Movil{
         this.ram = ram;
         this.bateria = bateria;
         this.camara = camara;
-        this.precio=precio;
+        this.precio = precio;
         this.propietario = null;
         this.votos = [];
     }
 
-    get id(){
+    get id() {
         return this._id;
     }
 
-    set id(id){
-        this._id = id; 
+    set id(id) {
+        this._id = id;
     }
 
-    get modelo(){
+    get modelo() {
         return this._modelo;
     }
 
-    set modelo(modelo){
-        this._modelo = modelo; 
+    set modelo(modelo) {
+        this._modelo = modelo;
     }
 
-    get marca(){
+    get marca() {
         return this._marca;
     }
 
-    set marca(marca){
-        this._marca = marca; 
+    set marca(marca) {
+        this._marca = marca;
     }
 
-    get pantalla(){
+    get pantalla() {
         return this._pantalla;
     }
 
-    set pantalla(pantalla){
-        this._pantalla = pantalla; 
+    set pantalla(pantalla) {
+        this._pantalla = pantalla;
     }
 
-    get rom(){
+    get rom() {
         return this._rom;
     }
 
-    set rom(rom){
-        this._rom = rom; 
+    set rom(rom) {
+        this._rom = rom;
     }
-    get ram(){
+    get ram() {
         return this._ram;
     }
 
-    set ram(ram){
-        this._ram = ram; 
+    set ram(ram) {
+        this._ram = ram;
     }
-    
-    get bateria(){
+
+    get bateria() {
         return this._bateria;
     }
 
-    set bateria(bateria){
-        this._bateria = bateria; 
+    set bateria(bateria) {
+        this._bateria = bateria;
     }
-    
-    get camara(){
+
+    get camara() {
         return this._camara;
     }
 
-    set camara(camara){
-        this._camara = camara; 
+    set camara(camara) {
+        this._camara = camara;
     }
-    get precio(){
+    get precio() {
         return this._precio;
     }
 
-    set precio(precio){
-        this._precio = precio; 
+    set precio(precio) {
+        this._precio = precio;
     }
 
 
-    get persona(){
+    get persona() {
         return this._persona;
     }
 
-    set persona(persona){
-        this._persona = persona; 
+    set persona(persona) {
+        this._persona = persona;
     }
 
-    get votos(){
+    get votos() {
         return this._votos;
     }
 
-    set votos(votos){
-        this._votos = votos; 
+    set votos(votos) {
+        this._votos = votos;
     }
     Propietario
-    addVoto(voto){
+    addVoto(voto) {
         this.votos.push(voto);
     }
 }
 
-class Persona{
-    constructor(id,nombre,edad){
+class Persona {
+    constructor(id, nombre, edad) {
         this._id = id;
         this._nombre = nombre;
         this._edad = edad;
@@ -117,45 +117,45 @@ class Persona{
     }
 
 
-    get id(){
+    get id() {
         return this._id;
     }
 
-    set id(id){
-        this._id = id; 
+    set id(id) {
+        this._id = id;
     }
 
-    get nombre(){
+    get nombre() {
         return this._nombre;
     }
 
-    set modelo(nombre){
-        this._nombre = nombre; 
+    set modelo(nombre) {
+        this._nombre = nombre;
     }
 
-    get edad(){
+    get edad() {
         return this._edad;
     }
 
-    set edad(edad){
-        this._edad = edad; 
+    set edad(edad) {
+        this._edad = edad;
     }
 
-    get votos(){
+    get votos() {
         return this._votos;
     }
 
-    set votos(votos){
-        this._votos = votos; 
+    set votos(votos) {
+        this._votos = votos;
     }
 
-    addVoto(voto){
+    addVoto(voto) {
         this.votos.push(voto);
     }
 }
 
-class Voto{
-    constructor(id,persona,movil,puntuacion,observaciones){
+class Voto {
+    constructor(id, persona, movil, puntuacion, observaciones) {
         this.id = id;
         this.persona = persona;
         this.movil = movil;
@@ -163,50 +163,50 @@ class Voto{
         this.observaciones = observaciones;
     }
 
-    get id(){
+    get id() {
         return this._id;
     }
 
-    set id(id){
-        this._id = id; 
+    set id(id) {
+        this._id = id;
     }
 
-    get persona(){
+    get persona() {
         return this._persona;
     }
 
-    set persona(persona){
-        this._persona = persona; 
+    set persona(persona) {
+        this._persona = persona;
     }
 
-    get movil(){
+    get movil() {
         return this._movil;
     }
 
-    set movil(movil){
-        this._movil = movil; 
+    set movil(movil) {
+        this._movil = movil;
     }
 
-    get puntuacion(){
+    get puntuacion() {
         return this._puntuacion;
     }
 
-    set puntuacion(puntuacion){
-        this._puntuacion = puntuacion; 
+    set puntuacion(puntuacion) {
+        this._puntuacion = puntuacion;
     }
 
-    get observaciones(){
+    get observaciones() {
         return this._observaciones;
     }
 
-    set observaciones(observaciones){
-        this._observaciones = observaciones; 
+    set observaciones(observaciones) {
+        this._observaciones = observaciones;
     }
 }
 
-function crearVoto(persona,movil,puntuacion,observaciones){
-    let idVoto = listaTodosLosVotos.length +1;
-    let voto = new Voto(idVoto,persona,movil,puntuacion,observaciones);
+function crearVoto(persona, movil, puntuacion, observaciones) {
+    let idVoto = listaTodosLosVotos.length + 1;
+    let voto = new Voto(idVoto, persona, movil, puntuacion, observaciones);
     persona.addVoto(voto);
     movil.addVoto(voto);
     listaTodosLosVotos.push(voto);
@@ -214,8 +214,8 @@ function crearVoto(persona,movil,puntuacion,observaciones){
 
 let listaTodosLosVotos = [];
 
-let Andresito = new Persona(1,"Andresito",18);
-let Alfredito = new Persona(2,"Alfredito",23);
+let Andresito = new Persona(1, "Andresito", 18);
+let Alfredito = new Persona(2, "Alfredito", 23);
 
 let listaUsuarios = [];
 listaUsuarios.push(Andresito);
@@ -223,16 +223,16 @@ listaUsuarios.push(Alfredito);
 
 //constructor(id,modelo,marca,pantalla,rom,ram,bateria,camara){
 
-let n3310 = new Movil(1,"n3310","Nokia",3,1,1,500,1,20);
-let S10 = new Movil(2,"S10","Samsung",8,64,8,3000,32,500);
-let Xpro = new Movil(3,"Xpro","Apple",10,128,16,4000,35,800);
-let RedmiNote7 = new Movil(4,"RedmiNote7","Xiaomi",7,64,8,3500,45,350);
-let P30 = new Movil(5,"P30","Huawuei",9,64,16,3000,64,350);
-let OneTouch = new Movil(6,"OneTouch","Alcatel",2,1,1,400,1,20);
-let Zblade = new Movil(8,"Zblade","Motorola",5,64,8,3500,64,600);
-let Xtrem = new Movil(9,"Xtrem","Razer",4,64,8,5000,64,500);
-let PowerCell = new Movil(10,"PowerCell","Duracell",3,64,8,15000,64,400);
-let Poor = new Movil(11,"Poor","ZTE",4,32,4,3000,15,120);
+let n3310 = new Movil(1, "n3310", "Nokia", 3, 1, 1, 500, 1, 20);
+let S10 = new Movil(2, "S10", "Samsung", 8, 64, 8, 3000, 32, 500);
+let Xpro = new Movil(3, "Xpro", "Apple", 10, 128, 16, 4000, 35, 800);
+let RedmiNote7 = new Movil(4, "RedmiNote7", "Xiaomi", 7, 64, 8, 3500, 45, 350);
+let P30 = new Movil(5, "P30", "Huawuei", 9, 64, 16, 3000, 64, 350);
+let OneTouch = new Movil(6, "OneTouch", "Alcatel", 2, 1, 1, 400, 1, 20);
+let Zblade = new Movil(8, "Zblade", "Motorola", 5, 64, 8, 3500, 64, 600);
+let Xtrem = new Movil(9, "Xtrem", "Razer", 4, 64, 8, 5000, 64, 500);
+let PowerCell = new Movil(10, "PowerCell", "Duracell", 3, 64, 8, 15000, 64, 400);
+let Poor = new Movil(11, "Poor", "ZTE", 4, 32, 4, 3000, 15, 120);
 let listaMoviles = [];
 
 listaMoviles.push(n3310);
@@ -246,12 +246,12 @@ listaMoviles.push(Xtrem);
 listaMoviles.push(PowerCell);
 listaMoviles.push(Poor);
 
-crearVoto(Andresito,n3310,true,"Indestructible");
-crearVoto(Andresito,S10,true,"Ok");
-crearVoto(Andresito,Xpro,false,"Hype");
+crearVoto(Andresito, n3310, true, "Indestructible");
+crearVoto(Andresito, S10, true, "Ok");
+crearVoto(Andresito, Xpro, false, "Hype");
 
-crearVoto(Alfredito,P30,true,"Camaron");
-crearVoto(Alfredito,OneTouch,true,"Retro");
-crearVoto(Alfredito,Zblade,false,"Plantalla plegable");
+crearVoto(Alfredito, P30, true, "Camaron");
+crearVoto(Alfredito, OneTouch, true, "Retro");
+crearVoto(Alfredito, Zblade, false, "Plantalla plegable");
 
 
--- src/usuarios.js
+++ src/usuarios.js
@@ -1,22 +1,38 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
     crearSelectUsuario("selectUsuario");
     crearSelectMoviles("selectMovilVotar");
-    
+
     let selectUsuario = document.getElementById("selectUsuario");
-    selectUsuario.addEventListener("change",obtenerMovilesVotados);
-    
+    selectUsuario.addEventListener("change", obtenerMovilesVotados);
+
     let selectMovilVotar = document.getElementById("selectMovilVotar");
-    selectMovilVotar.addEventListener("change",validarMovil);
-    
+    selectMovilVotar.addEventListener("change", validarMovil);
+
     let formularioVotar = document.getElementById("formularioVotar");
-    formularioVotar.addEventListener("submit",votarMovil);
+    formularioVotar.addEventListener("submit", votarMovil);
 
-    botonRecargar.addEventListener("click",obtenerMovilesVotados);
+    botonRecargar.addEventListener("click", obtenerMovilesVotados);
+
+   /** let inputBatMin = document.getElementById("batBaja");
+    inputBatMin.addEventListener("keyup", function (event) {
+        comprobarCampo(event, comprobarNumero);
+    })
+    let inputBatAlta = document.getElementById("batAlta");
+    inputBatAlta.addEventListener("keyup", function (event) {
+        comprobarCampo(event, comprobarNumero);
+    }) */
+    // botonMuestraBateria.addEventListener("click",mostrarMovilesporBateria);
 })
+/**function comprobarCampo(event, funcionComprobar) {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+        funcionComprobar(event.target);
+    }, 500);
+}; */
 
-function crearSelectUsuario(idUsuario){
-    let select = document.getElementById(idUsuario); 
-    for(usuario of listaUsuarios){
+function crearSelectUsuario(idUsuario) {
+    let select = document.getElementById(idUsuario);
+    for (usuario of listaUsuarios) {
         let option = document.createElement("option");
         option.value = usuario.id;
         option.innerHTML = usuario.nombre;
@@ -24,9 +40,9 @@ function crearSelectUsuario(idUsuario){
     }
 }
 
-function crearSelectMoviles(idMovil){
-    let select = document.getElementById(idMovil); 
-    for(movil of listaMoviles){
+function crearSelectMoviles(idMovil) {
+    let select = document.getElementById(idMovil);
+    for (movil of listaMoviles) {
         let option = document.createElement("option");
         option.value = movil.id;
         option.innerHTML = movil.modelo;
@@ -34,60 +50,131 @@ function crearSelectMoviles(idMovil){
     }
 }
 
-function obtenerMovilesVotados(event){
+function obtenerMovilesVotados(event) {
     let selector = document.getElementById("selectUsuario");
     let idUsuarioSeleccionado = selector.value;
     let ulMovilesVotados = document.getElementById("moviles_votados");
     ulMovilesVotados.innerHTML = "";
     let spanNombreUsuario = document.getElementById("nombre_usuario");
     spanNombreUsuario.innerHTML = "";
-    if(idUsuarioSeleccionado !== ""){
-       // intentando que salgan los datos de puntuacion y observaciones
-      // let movilesVotados=listaTodosLosVotos;
-      let movilesVotados = [];
-        let persona = listaUsuarios.find( persona => persona.id === parseInt(idUsuarioSeleccionado));
+    if (idUsuarioSeleccionado !== "") {
+        // intentando que salgan los datos de puntuacion y observaciones
+        // let movilesVotados=listaTodosLosVotos;
+        let movilesVotados = [];
+        let persona = listaUsuarios.find(persona => persona.id === parseInt(idUsuarioSeleccionado));
         spanNombreUsuario.innerHTML = persona.nombre;
-        persona.votos.forEach( voto => movilesVotados.push(voto.movil)); 
-        for(let movilVotado of movilesVotados){
+        persona.votos.forEach(voto => movilesVotados.push(voto.movil));
+        for (let movilVotado of movilesVotados) {
             let liMovil = document.createElement("li");
-          //  liMovil.innerHTML = movilVotado.marca +" "+ movilVotado.modelo +" "+movilVotado.puntuacion;
-          liMovil.innerHTML = movilVotado.marca +" "+ movilVotado.modelo;
+            //  liMovil.innerHTML = movilVotado.marca +" "+ movilVotado.modelo +" "+movilVotado.puntuacion;
+            liMovil.innerHTML = movilVotado.marca + " " + movilVotado.modelo;
             ulMovilesVotados.appendChild(liMovil);
-        
+
         }
     }
 }
 
-function validarMovil(){
+function validarMovil() {
     let esCorrecto = true;
     let select = document.getElementById("selectMovilVotar");
     let selectUsuarioVotar = document.getElementById("selectUsuario");
     let idMovilVotar = select.value;
     let idUsuarioSeleccionado = selectUsuarioVotar.value;
-    let usuarioAVotar = listaUsuarios.find( persona => persona.id === parseInt(idUsuarioSeleccionado));
-    let movilAVotar = listaMoviles.find( movil => movil.id === parseInt(idMovilVotar));
-    let haVotado = usuarioAVotar.votos.some( voto => voto.movil.id === parseInt(idMovilVotar) );
-    if(haVotado){
+    let usuarioAVotar = listaUsuarios.find(persona => persona.id === parseInt(idUsuarioSeleccionado));
+    let movilAVotar = listaMoviles.find(movil => movil.id === parseInt(idMovilVotar));
+    let haVotado = usuarioAVotar.votos.some(voto => voto.movil.id === parseInt(idMovilVotar));
+    if (haVotado) {
         alert(`${usuarioAVotar.nombre} ya has votado por el movil ${movilAVotar.modelo}`)
     }
     return esCorrecto;
 }
 
-function votarMovil(event){
+function votarMovil(event) {
     event.preventDefault();
     let formulario = document.getElementById("formularioVotar");
     let esMovilCorrecto = validarMovil();
-    if(esMovilCorrecto){
+    if (esMovilCorrecto) {
         let selectUsuario = document.getElementById("selectUsuario");
         let selectMovil = document.getElementById("selectMovilVotar");
         let idUsuarioSeleccionado = selectUsuario.value;
         let idMovilVotar = selectMovil.value;
-        let usuarioAVotar = listaUsuarios.find( persona => persona.id === parseInt(idUsuarioSeleccionado));
-        let movilAVotar = listaMoviles.find( movil => movil.id === parseInt(idMovilVotar));
+        let usuarioAVotar = listaUsuarios.find(persona => persona.id === parseInt(idUsuarioSeleccionado));
+        let movilAVotar = listaMoviles.find(movil => movil.id === parseInt(idMovilVotar));
         let puntuacion = document.getElementById("puntuacion");
         let observaciones = document.getElementById("observaciones");
-        crearVoto(usuarioAVotar,movilAVotar,puntuacion,observaciones);
+        crearVoto(usuarioAVotar, movilAVotar, puntuacion, observaciones);
         formulario.reset();
         obtenerMovilesVotados();
     }
 }
+function comprobarNumero(inputBatMin) {
+    let correcto = false;
+    let expresion = /^[0-9]+$/g;
+
+    if (expresion.test(inputBatMin.value)
+    ) {
+        correcto = true;
+        /**let valor = parseInt(inputBatMin.value);
+        if(valor >= 6 && valor <=24){
+         //   marcarInputCorrecto(inputBatMin);
+            correcto = true;
+        }else{
+           // marcarInputErroneo(inputBatMin);
+           alert("Solo pueden introducirse números");
+        } */
+    } else {
+        // marcarInputErroneo(inputBatMin);
+        alert("Solo pueden introducirse números");
+    }
+    return correcto;
+}
+function comprobarNumero(inputBatAlta) {
+    let correcto = false;
+    let expresion = /^[0-9]+$/g;
+
+    if (expresion.test(inputBatAlta.value)
+    ) {
+        correcto = true;
+        /**let valor = parseInt(inputBatAlta.value);
+        if(valor >= 6 && valor <=24){
+           // marcarInputCorrecto(inputBatAlta);
+            correcto = true;
+        }else{
+           // marcarInputErroneo(inputBatMin);
+           alert("Solo pueden introducirse números");
+        } */
+    } else {
+        // marcarInputErroneo(inputBatMin);
+        alert("Solo pueden introducirse números");
+    }
+    return correcto;
+}
+
+/**function mostrarMovilesporBateria(event){
+    let minimoBat = inputBatMin.value;
+    let maximoBat= inputBatAlta.value;
+
+    let ulMovilesVotados = document.getElementById("bateria_moviles");
+    ulMovilesVotados.innerHTML = "";
+  /**  let spanNombreUsuario = document.getElementById("nombre_usuario");
+  //  spanNombreUsuario.innerHTML = "";
+
+       // intentando que salgan los datos de puntuacion y observaciones
+      // let movilesVotados=listaTodosLosVotos;
+      let movilesMostrados = [];
+       // let persona = listaUsuarios.find( persona =>
+       //persona.id === parseInt(idUsuarioSeleccionado));
+       // spanNombreUsuario.innerHTML = persona.nombre;
+       let movil=  new Movil;
+
+       movil.bateria.forEach( console.log(movil._bateria));
+
+         /**for(let movilesMostrados of movilesMostrados){
+             let liMovil = document.createElement("li");
+           //  liMovil.innerHTML = movilVotado.marca +" "+ movilVotado.modelo +" "+movilVotado.puntuacion;
+           liMovil.innerHTML = movilVotado.marca +" "+ movilVotado.modelo;
+           ulMovilesVotados.appendChild(liMovil);
+
+         }
+
+ } */
