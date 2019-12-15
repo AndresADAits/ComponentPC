233807f12c80895e37c8f9010f84362c8b2c8cba
JAndres <joanferfra@hotmail.com>
2019-12-15 16:29:35 +0100

    picoteo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ComponentPC
  TrabajoEntornoCliente
+
+ ### Tiempo empleado en cada funcion:
+
+ ##### Sinceramente no hemos medido especificamente cuanto tiempo nos ha tomado implementar cada funcion, si bien hemos tardado entre 4 y 6 horas las que más nos han costado, y entre 20 y 30 minutos las que nos han resultado más faciles. De todas formas se adjuntara los registros de una aplicación para medir el tiempo en el que puede apreciarse la gran cantidad de horas que hemos empleado en este trabajo.
--- src/gestion.js
+++ src/gestion.js
@@ -131,51 +131,42 @@ function obtieneValue() {
 function muestraModeloYFiltros() {
     let usuarioSeleccionado = $("#selectUsuario").val();
     let modeloSelecionado = $("#selectMovilModelo").val();
-  
-        let borrador = document.getElementById("muestraFavoritoYfiltro");
-        borrador.innerHTML = "";
-
-
 
     for (let voto of listaTodosLosVotos) {
+        let borrador = document.getElementById("muestraFavoritoYfiltro");
+        borrador.innerHTML = "";
 
         if (voto.persona.nombre === usuarioSeleccionado) {
 
             if (voto.movil.modelo === modeloSelecionado) {
 
-                if (voto.puntuacion === true) {
-                    /**
-                     * VACIO LO QUE PUDIESE ESTAR IMPRESO ANTES EN EL DIV QUE LUEGO VOY A IMPRIMIR.
-                     * 
-                     */
+                if (voto.puntuacion === 1) {
+                    //VACIO LO QUE PUDIESE ESTAR IMPRESO ANTES EN EL DIV QUE LUEGO VOY A IMPRIMIR.
+                    let borrador = document.getElementById("muestraFavoritoYfiltro");
+                    borrador.innerHTML = "";
 
-
-                    /**
-                    * CREO EL FILTRO PARA QUE MUESTRE MOVILES DEL MODELO SELECCIONADO
-                    */
-
+                    //CREO EL FILTRO PARA QUE MUESTRE MOVILES DEL MODELO SELECCIONADO.
                     let MovilesBienValorado = listaMoviles.filter(movil => modeloSelecionado === movil.modelo);
 
-                    /**
-                     * LLAMO A LA FUNCION DE FUNCIONES GENERALES QUE IMPRIME CON EL NOMBRE DEL DIV Y UN FILTRO COMO PARAMETROS./**
-                                         * CREO EL FILTRO PARA QUE MUESTRE MOVILES DEL MODELO SELECCIONADO
-                                         */
-
+                    //LLAMO A LA FUNCION DE FUNCIONES GENERALES QUE IMPRIME CON EL NOMBRE DEL DIV Y UN FILTRO COMO PARAMETROS.
                     imprimeDiv("muestraFavoritoYfiltro", MovilesBienValorado);
-                } else {
+
+
                     let borrando = document.createElement("li");
-                    borrando.innerHTML = "ESE USUARIO NO HA VALORADO BIEN A ESE MODELO o NO VOTO POR EL";
+                    borrando.innerHTML = "ESE USUARIO NO HA VALORADO BIEN A ESE MODELO O NO VOTO POR EL";
                     borrador.appendChild(borrando);
                 }
-            } else {
-                let borrando = document.createElement("li");
-                borrando.innerHTML = "ESE USUARIO NO HA VALORADO BIEN A ESE MODELO o NO VOTO POR EL";
-                borrador.appendChild(borrando);
             }
-        } else {
+        }
+        if (voto.persona.nombre !== usuarioSeleccionado || voto.movil.modelo !== modeloSelecionado || voto.puntuacion === 0) {
+            //VACIO LO QUE PUDIESE ESTAR IMPRESO ANTES EN EL DIV QUE LUEGO VOY A IMPRIMIR.
+            let borrador = document.getElementById("muestraFavoritoYfiltro");
+            borrador.innerHTML = "";
+            //AÑADO UN MENSAJE DE QUE NO SE VALORADO BIEN O NO HA SIDO VOTADO, POR EL USUARIO SELECCIONADO. 
             let borrando = document.createElement("li");
-            borrando.innerHTML = "ESE USUARIO NO HA VALORADO BIEN A ESE MODELO o NO VOTO POR EL";
+            borrando.innerHTML = "ESE USUARIO NO HA VALORADO BIEN A ESE MODELO O NO VOTO POR EL";
             borrador.appendChild(borrando);
+
         }
     }
 }
